5ba0a93992abc0294084d9711e2207a092019b29
Omonzejele Precious <crispusomonze@gmail.com>
2017-07-27 16:49:11 +0100

    Pushing all to the repo
    bla :)

---
+++ vendor/pp/curl/Curl.class.php
@@ -0,0 +1,99 @@
+<?php
+	
+	/*
+	* For handling curl requests
+	*/
+	
+	class Curl{
+	 
+    /**
+     * the curl holder
+     * @var object
+     */
+    private $curl = '';
+
+		 
+    /**
+     * return transfer
+     * @var int
+     */
+	private $return_transfer = 1;
+		
+    /**
+     * return transfer
+     * @var int
+     */
+	private $return_transfer = 1;
+	
+    /**
+     * the url
+     * @var string
+     */	
+	public $url = '';
+		
+	/**
+     * result
+     * @var string
+     */	
+	public $result = false;
+		
+		
+		public function __construct(){
+			//initialise
+			$this->curl = curl_init();
+		}
+	/**
+     * Sends a request to the specified url.
+     *
+     * @param string $url url to use.
+     *
+     * @return false - if there is an error executing the request, true - if the request executed without error and CURLOPT_RETURNTRANSFER is set to false, the result - if the request executed without error and CURLOPT_RETURNTRANSFER is set to true
+     */
+		public function get_auth($url){
+			$this->url = $url;
+			// do your thing
+			curl_setopt_array($this->curl,array(
+			CURLOPT_RETURNTRANSFER => $this->return_transfer,
+			CURLOPT_URL => $this->url
+			)
+			);
+			$this->result = curl_exec($this->curl);
+			
+			curl_close($this->curl);
+			
+			return $this->result;
+			
+		}
+		
+	/**
+     * Get an error if there's an error
+     *
+     * @return false if no error is found, or returns the error message.
+     */		
+		public function get_error(){
+			if(!curl_exec($this->curl)){
+				//return the error message
+				return curl_error($this->curl);
+			}
+			else{
+				return false;
+			}
+		}
+	/**
+     * Get an error number if there's an error
+     *
+     * @return false if no error is found, or returns the error number.
+     */		
+		public function get_errno(){
+			if(!curl_exec($this->curl)){
+				//return the error message
+				return curl_errno($this->curl);
+			}
+			else{
+				return false;
+			}
+		}
+		
+	}
+	
+	
---
+++ vendor/pp/session/Session.class.php
@@ -0,0 +1,110 @@
+ <?php
+	//namespace pp\mail;
+/*
+* Session stuffETC
+* 
+*/
+ class Session{
+	 
+	 /**
+     * determines if smpt is to be used or not.
+     * @var bool
+	 * @default true;
+     */
+	 
+	 private $use_smtp = true;
+	 
+	 
+	 /**
+     * body of the message, usually allows html tags and design
+     * @var string
+     */
+	 
+	 private $body = '';
+	 
+	 /**
+     * alt body of the message, plain text
+     * @var string
+     */
+	 
+	 private $alt_body = '';
+	 
+	 private $to ='';
+	
+	 /**
+     * For storing multiple users
+     * @var PHPMailer
+     */
+	 
+	 public $subject = '';
+	 
+	 /**
+     * For storing multiple users
+     * @var PHPMailer
+     */
+	 
+	 private $recipients = array();
+    /**
+     * Importing the php mailer.
+     * @var PHPMailer
+     */
+	 private $mail = '';
+	 
+    /**
+     * The HOST FOR SMTP PURPOSES.
+     * @var string
+     */
+	 public $host = '';
+	 
+    /**
+     * SMTP username.
+     * @var string
+     */
+    public $username = '';
+
+    /**
+     * SMTP password.
+     * @var string
+     */
+    public $password = '';
+
+    /**
+     * The From email address for the message.
+     * @var string
+     */
+	 public $from = '';
+	 
+    /**
+     * The From name for the message.
+     * @var string
+     */
+	 public $from_name = '';
+	 
+	 
+    /**
+     * Initialise the mailing stuff
+     * @param $use_smtp = 'true' (optional)
+     */
+	 
+	 public function __construct(){
+		 
+		 
+	 }
+	 
+	 /*
+	  * Checks session basically
+	  * @param string, the session to check for
+	  *
+	  * @return boolean, returns true if the session is valid, false otherwise.
+	  */
+    public function check_basic_session($session_id){
+		if(isset($session_id) && !empty($session_id)){
+			return true;
+		}
+		else{
+			return false;
+		}
+    }
+    
+	 }
+ 
