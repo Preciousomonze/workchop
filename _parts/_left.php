c1eb809fdf9ede457bcf567cd47964e1ed86ac3b
Omonzejele Precious <crispusomonze@gmail.com>
2017-08-16 00:32:48 +0100

    used the load image function for the profile picture

--- _parts/_left.php
+++ _parts/_left.php
@@ -4,15 +4,8 @@
 			<?php 
 			//get the user url picture
 			$pic_url = $site_url."mobile_app/user_pictures/".$user.".jpg";
-			$user_pic = $curl->get_auth($pic_url);
-			//echo $pic_url;
-			if($curl->get_error()){
-				$pic_url = USER_AREA."default.jpg";
-			}
-			else{
-				//echo "yh";
-			}
-			?>
+				$pic_url = load_image($pic_url);
+		?>
 				<img class="profile-picture" src="<?php echo $pic_url; ?>" alt="<?php echo $firstname ." ".$surname; ?>">
 			<p class="their-names"><?php echo $firstname ." ".$surname; ?></p>
 			</div> 
