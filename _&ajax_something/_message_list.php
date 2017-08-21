0ce4dfcfe61b7a9936e81b082ed89bd886007f97
Omonzejele Precious <crispusomonze@gmail.com>
2017-08-21 13:12:21 +0100

    added footer stuff

--- _&ajax_something/_message_list.php
+++ _&ajax_something/_message_list.php
@@ -20,26 +20,28 @@
 	 exit();
  }
  //get the request
-$url = $site_url."mobile_app/get_user_chat_list.php?user_id=".$user."";
+$url = $site_url."mobile_app/get_user_chat_list2.php?user_id=".$user."";
  $curl = new Curl();
  
  $result = $curl->get_auth($url);
  if($curl->get_error()){
-	
+		 //no message
+		echo toast_note("An error occured, please try again later.",2); 
+		 exit();
  }
  else{
 	 //no error
 	 $messages = explode($chat_delimiter,$result);
 	 if(count($messages) < 1){
 		 //no message
-		echo "bla bla"; 
-		 exit();
+		echo alert_note("No Message Thread. Start a conversation."); 
+		 exit(); 
 	 }
 	 //now loop
 	 for($i = 0; $i < count($messages); $i++){
 		 //get values
 		 $message_details = explode($delimiter,$messages[$i]);
-		 $t_id = $m_details[0];
+		 $t_id = trim($m_details[0]);
 		 $t_name = $m_details[1];
 		 $date = $m_details[3];
 		 $unread_no = (int)$m_details[4];
@@ -52,6 +54,7 @@ $url = $site_url."mobile_app/get_user_chat_list.php?user_id=".$user."";
 			 $badge_no = "	<span class=\"wc-badge pull-right\">".$unread_no."</span>";
 		 }
 		 ?>
+		 <a href="#open-message" onclick="showMessage($t_id)" title="view conversation with <?php $t_name; ?>">
 		 <div class="message-box <?php echo $is_unread; ?>">
 						<section class="message-body">
 							<div class="row">
@@ -65,7 +68,33 @@ $url = $site_url."mobile_app/get_user_chat_list.php?user_id=".$user."";
 									 <?php echo $t_name; ?>
 									</p>
 									<p class="message-part">
-									 bla bla bla bla bla bla la something
+									<?php // now load the most recent message into this part
+									
+									 $url = $site_url."mobile_app/get_user_chats.php?vendor_id=".$vendor_id."&user_id=".$user."";
+  
+ 
+ $curl = new Curl();
+ 
+ $result = trim(strtolower($curl->get_auth($url)));
+ //var_dump($result);
+ //check if theres an error
+ if(!$curl->get_error() || $result != "false"){
+	 //get the list of messages.
+	 $messages = explode('------',$result);
+	 ///now loop through the result to get necessary info
+	
+		//end of list, so that we start from the bottom
+		$end_list = count($messages) - 1 ;
+		//store the last message.
+		$last_message = '';
+		$last_message_set = '';
+		for($i = $end_list; $i >=0; $i--){
+			$last_message_set = explode($delimiter,$messages[$i]);
+			$last_message = $last_message_set[3];
+			break;
+		}
+		echo $last_message;
+									?>
 									</p>
 								</div><?php //col
 								?>
@@ -76,13 +105,19 @@ $url = $site_url."mobile_app/get_user_chat_list.php?user_id=".$user."";
 							</div>
 						</section>
 						<section class="detail-section">
-							<i class="icon fa-clock-o"></i> <span title="time">time</a>
+							<i class="icon fa-clock-o"></i> <span title="time"><?php echo $last_message_set[4]; ?></a>
 						</section>
 						
 					</div>
 					
+					</a>
+					
 		 <?php
 	 } 
  }
+ 
+ }
 
  ?>
+ 
+ 
---
+++ _parts/_footer.php
@@ -0,0 +1,7 @@
+<?php //footer ?> 
+ 
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script> 
+<script type="text/javascript" src="<?php echo base_url("assets/smooth-scroll/smooth-scroll.js"); ?>"></script> 
+ 
+</body> 
+</html>
